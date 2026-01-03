c8bf34637fa05bd4455d102253f8da7b56a1cfdd
reniroma <bookmaker_reni@hotmail.com>
2026-01-03 23:58:24 +0100

    Update _rateLimit.js

--- backend/api/_rateLimit.js
+++ backend/api/_rateLimit.js
@@ -1,9 +1,10 @@
 // backend/api/_rateLimit.js
 "use strict";
 
-const { Redis } = require("@upstash/redis");
-
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 const RATE_LIMIT_LUA = `
   local key = KEYS[1]
